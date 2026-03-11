a56c2f5f142cb994b9881262cf128dab82749416
solracs <csalase@protonmail.com>
2026-03-11 18:34:56 +0000

    Update (forgotten long time ago...)

--- script.js
+++ script.js
@@ -3,30 +3,52 @@
 //chall01
 function chall01_solver() {
 	Java.perform( () => {
-		const challenge_01 = Java.use('uk.rossmarks.fridalab.challenge_01');
-		challenge_01.getChall01Int.implementation = () => {
-			console.log('[*] challenge_01.getChall01Int overloaded');
-			return 1;
+		var challenge_01 = Java.use('uk.rossmarks.fridalab.challenge_01');
+		console.log('[*] Hooked challenge 01');
+		var v = challenge_01.getChall01Int();
+		console.log('[*] 01 value: ' + v);
+		challenge_01.chall01.value = 1;
+		v = challenge_01.getChall01Int();
+		console.log('[*] New 01 value: ' + v);
 		}
-	});
+	)};
+
+//chall02
+function chall02_solver(main) {
+			main.chall02();
 }
 
-function chall02_solver() {
-	Java.perform( () => {
-		Java.choose('uk.rossmarks.fridalab.MainActivity', {
-			onMatch: (instance) => {
-				console.log('[*] Found a MainActivity instance');
-				console.log('\t[+] Calling chall02()')
-				instance.chall02();
-			},
-			onComplete: () => {}
-		});
-	});
+//chall03
+function chall03_solver(main) {
+			main.chall03.overload().implementation = () => {
+				console.log('[*] Changing chall03 implementation.');
+				return true;
+			};
+}
+
+//chall04
+function chall04_solver(main) {
+			main.chall04('frida');
 }
 
 function main(){
-	chall01_solver();
-	chall02_solver();
+	setTimeout( () => {
+		var main;
+		Java.perform( () => {
+			Java.choose('uk.rossmarks.fridalab.MainActivity', {
+                onMatch: function(instance) {
+					console.log('[*] Hooked challenge 02');
+    				main = instance;
+                },
+                onComplete: function() {}
+            });
+		});
+		chall01_solver();
+		chall02_solver(main);
+		chall03_solver(main);
+		chall04_solver(main);
+	}, 2000);
+
 }
 
 main();
